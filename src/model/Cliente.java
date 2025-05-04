f5a44bebdadbfee7944d43e668d081009df5415b
Cesarcardozo11 <cesar.andres.cardozo.espitia@gmail.com>
2025-05-04 08:52:02 -0500

    Se implemento el contador

---
+++ src/model/Cliente.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Cliente {
+    private String numeroIdentificacion;
+    private String nombre;
+    private String direccion;
+
+    public Cliente(String numeroIdentificacion, String nombre, String direccion) {
+        this.numeroIdentificacion = numeroIdentificacion;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    // Getters y setters
+    public String getNumeroIdentificacion() {
+        return numeroIdentificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+               "numeroIdentificacion='" + numeroIdentificacion + '\'' +
+               ", nombre='" + nombre + '\'' +
+               ", direccion='" + direccion + '\'' +
+               '}';
+    }
+}
