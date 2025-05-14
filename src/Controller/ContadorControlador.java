9f5920815874b29b0dc670237453742cf54ed3e5
Cesarcardozo11 <cesar.andres.cardozo.espitia@gmail.com>
2025-05-14 17:26:31 -0500

    ups

--- src/Controller/ContadorControlador.java
+++ src/Controller/ContadorControlador.java
@@ -46,7 +46,6 @@ public class ContadorControlador {
     }
 
     public void ejecutar() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'ejecutar'");
     }
 
